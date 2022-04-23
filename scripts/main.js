e549fe888b610e7e773983e2f43398376e4a12ec
BDF007 <bdf007@gmail.com>
2022-04-23 15:06:29 +0200

    appli finalisé :)

--- scripts/main.js
+++ scripts/main.js
@@ -10,6 +10,10 @@ const temperature = document.querySelector(".temperature");
 const localisation = document.querySelector(".localisation");
 const heure = document.querySelectorAll(".heure-nom-prevision");
 const tempPourH = document.querySelectorAll(".heure-prevision-valeur");
+const joursDiv = document.querySelectorAll(".jour-prevision-nom");
+const tempJoursDiv = document.querySelectorAll(".jour-prevision-temp");
+const imgIcone = document.querySelector(".logo-meteo");
+const chargementContainer = document.querySelector(".overlay-icone-chargement");
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
@@ -64,5 +68,28 @@ function AppelAPI(long, lat) {
           resultatsAPI.hourly[j * 3].temp
         )}°`;
       }
+
+      //trois premieres lettres des jours
+
+      for (let k = 0; k < tabJourEnOrdre.length; k++) {
+        joursDiv[k].innerText = tabJourEnOrdre[k].slice(0, 3);
+      }
+
+      //   temperature par jour
+
+      for (let m = 0; m < 7; m++) {
+        tempJoursDiv[m].innerText = `${Math.trunc(
+          resultatsAPI.daily[m + 1].temp.day
+        )}°`;
+      }
+
+      //   icone dynamique
+      if (heureActuelle >= 6 && heureActuelle < 21) {
+        imgIcone.src = `ressources/jour/${resultatsAPI.current.weather[0].icon}.svg`;
+      } else {
+        imgIcone.src = `ressources/nuit/${resultatsAPI.current.weather[0].icon}.svg`;
+      }
+
+      chargementContainer.classList.add("disparition");
     });
 }
